6b34c84344b9ceaced2b4e5ab4fd805beee3124d
stardust-hu <angelofhu@foxmail.com>
2016-02-28 09:57:15 +0800

    decrease CPU

--- weibo.py
+++ weibo.py
@@ -88,6 +88,8 @@ if __name__ == '__main__':
 	w = weibo(username, password)
 	while(1):
 		t = time.localtime()	
-		while (t.tm_min == 0):
+		if ((t.tm_min == 0) and (t.tm_sec == 0)):
 			text = u'#打更机器人#当前时间%d年%d月%d日%d时%d分%d秒' % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
 			w.update(text)
+		time.sleep(3530)	# 59*60-10
+
